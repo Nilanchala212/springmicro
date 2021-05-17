f23ff157f37a0f93f78520bf1ba64ca46a6cf7e7
Nilanchala <nilanchala212@gmail.com>
2021-05-17 13:54:11 +0530

    microservice test

---
+++ amazon-service/src/main/java/com/neosoft/service/controller/PaymentController.java
@@ -0,0 +1,19 @@
+package com.neosoft.service.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment-provider")
+public class PaymentController {
+	
+	
+	@GetMapping("/payNow/{price}")
+	public String PayNow(@PathVariable int price) {
+		return "Pay with "+price+" Successfully";
+	}
+
+}
